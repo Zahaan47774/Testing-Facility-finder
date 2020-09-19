75aea84baaa36ac28467fce0da96fcfe2873828e
rahulReddyDev <68431863+rahulReddyDev@users.noreply.github.com>
2020-09-19 19:20:43 +0530

    Update StringHandling.java

--- CovidTestingCenter/StringHandling.java
+++ CovidTestingCenter/StringHandling.java
@@ -1,22 +1,20 @@
+import java.io.File;
 import java.util.ArrayList;
 
 public class StringHandling {
   public static void main(String[] args) {
+    File file = new File("Data Hackathon.txt");
     ArrayList<TestingCenter> testingCenters = new ArrayList<TestingCenter>();
-    ArrayList<TestingCenter> hospitals = new ArrayList<TestingCenter>();
     ArrayList<String> centerData = new ArrayList<String>();
-    centerData = InputArrayList.createArrayList("Data Hackathon.txt");
+    centerData = InputArrayList.createArrayList(file);
     for (String testingCenter : centerData) {
       String[] center = testingCenter.split(",");
-      TestingCenter testCenter = new TestingCenter(center[0], center[1], center[2], center[3], center[4], center[5]);
-      if(center[0].equals("Hospital")) {
-        hospitals.add(testCenter);
-      } else {
-        testingCenters.add(testCenter);
-      }
+      TestingCenter testCenter = new TestingCenter(center[0], center[1], center[2], center[3], center[4]);
+      testingCenters.add(testCenter);
     }
-    for (TestingCenter center : testingCenters) {
-      System.out.println(center);
+    for (String center : centerData) {
+      System.out.println(centerData);
     }
+    System.out.println(testingCenters);
   }
 }
